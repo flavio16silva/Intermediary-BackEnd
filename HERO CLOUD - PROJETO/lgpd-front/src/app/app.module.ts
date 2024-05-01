c5e4635b5525b753b6d37513652428e5d403b28a
Flavio Silva <fal_silva@hotmail.com>
2024-05-01 20:50:00 -0300

    Correção de dependencias

--- HERO CLOUD - PROJETO/lgpd-front/src/app/app.module.ts
+++ HERO CLOUD - PROJETO/lgpd-front/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { SharedModule } from "./shared/shared.module";
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
